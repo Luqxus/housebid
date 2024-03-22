f00362c8cd3184d120f9705fe7277d0445897ace
luqxus <lebelwane@protonmain.com>
2024-03-22 19:45:12 +0200

    APIFunc | handlerFunc | APIServer struct - Run Server Func

--- api.go
+++ api.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
+
+
+
+type APIFunc func(writer http.ResponseWriter, request *http.Request) error
+
+type APIServer struct {
+	mux *http.ServeMux
+	listenAddress string
+}
+
+func NewAPIServer(listenAddress string) *APIServer {
+	return &APIServer{
+		mux: http.NewServeMux(),
+		listenAddress: listenAddress,
+	}
+}
+
+func (s *APIServer) Run() error {
+
+//	start server and listen for requests
+	return http.ListenAndServe(s.listenAddress, s.mux)
+}
+
+func handlerFunc(fn APIFunc) http.HandlerFunc {
+//	handle incoming request
+	return func(writer http.ResponseWriter, request *http.Request) {
+
+//		timeout  context
+		ctx, cancel := context.WithTimeout(context.Background(), 30 *time.Second)
+
+//		cancel context
+		defer cancel()
+
+//		handler helper
+		err := fn(writer, request.WithContext(ctx))
+
+//		on error panic
+		log.Panic(err)
+	}
+}
